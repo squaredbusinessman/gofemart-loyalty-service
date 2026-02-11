b9944ba3a456690cd16af278130837da20862d3e
EVGENY ANTROPOV <burdaonelove@gmail.com>
2026-02-11 12:19:23 +0300

    feat: реализует хэширование и проверку пароля + тесты

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/pressly/goose/v3 v3.26.0
 	github.com/stretchr/testify v1.11.1
 	go.uber.org/zap v1.27.1
+	golang.org/x/crypto v0.40.0
 )
 
 require (
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,1 +1,27 @@
 package auth
+
+import (
+	"errors"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("hash password: %w", err)
+	}
+	return string(hash), nil
+}
+
+func CheckPassword(passwordHash string, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return ErrInvalidCredentials
+		}
+		return fmt.Errorf("check password: %w", err)
+	}
+	return nil
+}
