f017209fa8055150794c82fc2a19006114e3cde5
EVGENY ANTROPOV <burdaonelove@gmail.com>
2026-02-17 09:35:25 +0300

    feat: добавляет модели запросов и ответов по балансу и списаниям

--- internal/model/model.go
+++ internal/model/model.go
@@ -30,3 +30,19 @@ type OrderForAccrual struct {
 	UserID int64  `json:"user_id"`
 	Status string `json:"status"`
 }
+
+type BalanceResponse struct {
+	Current   float64 `json:"order"`
+	Withdrawn float64 `json:"withdrawn"`
+}
+
+type WithdrawRequest struct {
+	Order string  `json:"order"`
+	Sum   float64 `json:"sum"`
+}
+
+type Withdrawal struct {
+	Order       string    `json:"order"`
+	Sum         float64   `json:"sum"`
+	ProcessedAt time.Time `json:"processed_at"`
+}
